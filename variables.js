ade412d63c21f50458e23f7917258d9926f32c0f
Erin Black <erinblackdesign@gmail.com>
2018-06-03 12:09:12 -0500

    using ... to bundle all arguments into an array

--- variables.js
+++ variables.js
@@ -23,3 +23,26 @@ funcs[3]();
   console.log('a',a);
   // a = 42 would also produce an error because you can't adjust the assignment of the variable
 }
+
+{
+  const a =[2,3,4];
+  let b = [1, ...a, 5];
+  console.log('b',b);
+}
+
+{
+  function foo(x,y, ...z){
+    console.log(x,y,z);
+    // this is essentially gathering all missing objects and bundling them into an array called z
+    // z = [3,4,5,6];
+  }
+  foo(1,2,3,4,5,6);
+}
+
+{
+  function arguments(...args){
+    console.log('args', args);
+  }
+  arguments(2,6,8,9,4,'this is a string');
+  // bundles all arguments given into an array
+}
